d69182f6e81c6f27ce2ff6bda8997fe66fec6f44
tanta1ize <631350716@qq.com>
2026-01-13 15:40:52 +0800

    chore(project): 初始化企业私有云存储项目结构
    - 搭建SpringBoot基础项目骨架

---
+++ storage-backend/src/main/java/org/example/storagebackend/StorageBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.storagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageBackendApplication.class, args);
+    }
+
+}
